7fdf7310c23b3e4060e9cd42e7b764452e58a133
은지프로젝트 <dmswlwnsgk@gmail.com>
2023-10-24 16:21:48 +0900

    추가

--- React/src/main/java/com/example/demo/persistence/UserRepository.java
+++ React/src/main/java/com/example/demo/persistence/UserRepository.java
@@ -11,5 +11,5 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
   Boolean existsByUsername(String username);
   UserEntity findByUsernameAndPassword(String username, String password);
   boolean existsByemail(String email);
-  //추가
+  //추가10/24
 }
